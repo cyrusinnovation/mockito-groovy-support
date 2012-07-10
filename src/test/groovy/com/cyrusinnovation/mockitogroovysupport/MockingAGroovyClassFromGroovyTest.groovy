5ce56ed852656010b7eb81690f421d76ca9e1bda
Laura and Moss <mockito-groovy-developers@cyruslists.com>
2012-07-10 15:38:53 -0400

    Added a test for Issue #1 -- argument matchers don't work.

--- src/test/groovy/com/cyrusinnovation/mockitogroovysupport/MockingAGroovyClassFromGroovyTest.groovy
+++ src/test/groovy/com/cyrusinnovation/mockitogroovysupport/MockingAGroovyClassFromGroovyTest.groovy
@@ -6,6 +6,7 @@ import org.junit.Test
 
 import static com.cyrusinnovation.mockitogroovysupport.MockitoGroovy.gmock
 import static org.junit.Assert.assertEquals
+import static org.mockito.Matchers.anyString
 import static org.mockito.Mockito.verify
 import static org.mockito.Mockito.when
 
@@ -13,6 +14,10 @@ class MockingAGroovyClassFromGroovyTest {
     static class SomeGroovyClass {
         String greeting() {
             return "Hello, world"
+        }
+
+        String methodTakingArgument(String argument) {
+            return "You said: $argument"
         }
     }
 
@@ -30,9 +35,25 @@ class MockingAGroovyClassFromGroovyTest {
     }
 
     @Test
-    @Ignore // WIP
+    @Ignore // See Issue #2
     void shouldBeAbleToVerifyAMethodOnAGroovyClass() {
         mock.greeting()
         verify(mock).greeting()
+    }
+
+    @Test
+    void shouldBeAbleToStubAMethodWithDifferentArguments() {
+        when(mock.methodTakingArgument("foo")).thenReturn("FOO")
+        when(mock.methodTakingArgument("bar")).thenReturn("BAR")
+
+        assertEquals("FOO", mock.methodTakingArgument("foo"))
+        assertEquals("BAR", mock.methodTakingArgument("bar"))
+    }
+
+    @Test
+    @Ignore // See Issue #1
+    void shouldBeAbleToStubAMethodUsingAnArgumentMatcher() {
+        when(mock.methodTakingArgument(anyString())).thenReturn("ANY STRING")
+        assertEquals("ANY STRING", mock.methodTakingArgument("whatever"))
     }
 }
